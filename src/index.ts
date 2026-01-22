0badae03f8380099c1c8e433113545d9e33bdc5e
sumrit <sumritarora.512@gmail.com>
2026-01-22 18:45:26 +0530

    ch

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://task-manger-theta-two.vercel.app"],
   credentials: true
 }));
 
