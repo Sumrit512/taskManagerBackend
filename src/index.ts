fa4aaa6732204d2ca273203cf71c42fe9ad04717
sumrit <sumritarora.512@gmail.com>
2026-01-22 17:09:37 +0530

    change

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,22 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://task-manger-theta-two.vercel.app/"
+];
+
 app.use(cors({
-  origin: "http://localhost:3000",
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
 }));
 
 
